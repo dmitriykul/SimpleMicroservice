d6a1111213ef71818f19fcdfeadfe57c4cb86ba0
Dmitry <kulik.dmitriy.2013@yandex.ru>
2021-04-09 11:03:49 +0700

    200

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"SimpleMicroservice/pkg/transport"
-	"fmt"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 )
@@ -15,11 +12,11 @@ type expression struct {
 const serverUrl = ":8000"
 
 func main() {
-	log.SetFormatter(&log.JSONFormatter{})
-	log.SetOutput(os.Stdout)
-	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
-	r := transport.Router()
-	fmt.Println(http.ListenAndServe(serverUrl, r))
+	//log.SetFormatter(&log.JSONFormatter{})
+	//log.SetOutput(os.Stdout)
+	//log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
+	//r := transport.Router()
+	//fmt.Println(http.ListenAndServe(serverUrl, r))
 
 	port := os.Getenv("PORT")
 
