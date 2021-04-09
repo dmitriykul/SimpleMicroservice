8af284f0e501dc4503d558bdc79c75aa661458f2
Dmitry <kulik.dmitriy.2013@yandex.ru>
2021-04-09 08:58:06 +0700

    PORT

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 
+	http.HandleFunc("/hello", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(w, "World")
+	})
+	http.ListenAndServe(":"+port, nil)
 }
